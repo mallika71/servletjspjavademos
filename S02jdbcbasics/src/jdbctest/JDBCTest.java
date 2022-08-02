0bae072d83a7991dfc377a742b591dc0c9636bca
mallika <mallikakakollu73954@gmail.com>
2022-08-02 20:46:21 +0530

    changes

--- S02jdbcbasics/src/jdbctest/JDBCTest.java
+++ S02jdbcbasics/src/jdbctest/JDBCTest.java
@@ -9,46 +9,44 @@ import java.sql.Statement;
 public class JDBCTest {
 	
 	public static void main(String[] args) {
+		readFromDB();
 
-		Connection connection = null;
-
-		Statement statement = null;
-
-		try {
-
-		connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "harsha", "harsha");
-
-		statement = connection.createStatement(); 
-		ResultSet resultSet =statement.executeQuery("select * from account");
-
-		while (resultSet.next()) {
-
-			System.out.println(resultSet.getInt(1) + ", "+resultSet.getString(2)+", " +resultSet.getString(3) + " , "+resultSet.getInt(4));
-		}
-		} catch (SQLException e) {
-
-			e.printStackTrace();
-
-		} finally {
-
-			try {
-
-				statement.close();
-
-				connection.close();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-
-			
-		}
-
-
+//		insertIntoDB();
 		
+//		updateDB();
+		
+//		deleteFromDB();   
+		
+		// CRUD - Create, Read, Update, Delete
+		
+		// update account set balance=150000 where accno=3
+		
+		// delete from account where accno=1
+		
+	}
+	
+	private static void insertIntoDB() {
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "harsha", "harsha");
+				Statement statement = connection.createStatement();) {
 
-		}
-
+			int rowsInserted = statement.executeUpdate("insert into account values(3, 'Mary', 'Lucy', 150000)");
+			System.out.println("Number of rows inserted: " + rowsInserted);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
 	}
 
-		
+	private static void readFromDB() {
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "harsha", "harsha");
+				Statement statement = connection.createStatement();) {
+
+			ResultSet resultSet = statement.executeQuery("select * from account");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt(1) + ", " + resultSet.getString(2) + ", " + resultSet.getString(3)
+						+ ", " + resultSet.getInt(4));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
 }
